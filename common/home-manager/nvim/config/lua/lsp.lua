5ce956bb173fe357ab99a31d8efe57f7cd6625a1
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-03-23 21:04:48 +0100

    chore(nvim): exclude gi keybinding from lsp config

--- common/home-manager/nvim/config/lua/lsp.lua
+++ common/home-manager/nvim/config/lua/lsp.lua
@@ -3,7 +3,11 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        -- TODO: remap vim.lsp.buf.implementation()
+        exclude = { 'gi' },
+    })
     lsp_zero.buffer_autoformat()
 end)
 

856e0dc74d0c3e84cbe5faa82de2ea26a0fccb6f
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-05-07 20:27:15 +0200

    feat(laptop): enable aarch64 emulation

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -32,6 +32,7 @@
   '';
 
   boot = {
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
     kernelParams = [ "nohibernate" ];
     loader.grub = {

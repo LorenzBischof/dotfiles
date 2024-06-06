9c9760016cfe5f96f20d83dc6639929ccf15177f
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-06-06 21:53:25 +0200

    chore: make direnv output less verbose

--- common/home-manager/shell/default.nix
+++ common/home-manager/shell/default.nix
@@ -8,6 +8,9 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      config.global = {
+        hide_env_diff = true;
+      };
     };
     starship = {
       enable = true;

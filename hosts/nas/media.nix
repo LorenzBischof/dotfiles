fcaf00f0a8c0d9c8243a17c7cd3415b88bc8efc8
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-10-06 22:08:00 +0200

    nas: add media stack

--- hosts/nas/media.nix
+++ hosts/nas/media.nix
@@ -11,11 +11,40 @@
       group = "media";
       mode = "0770";
     };
+    "/data/tv".d = {
+      group = "media";
+      mode = "0770";
+    };
   };
 
-  services.jellyfin = {
-    enable = true;
-    group = "media";
+  services = {
+    jellyfin = {
+      enable = true;
+      group = "media";
+    };
+    radarr = {
+      enable = true;
+      group = "media";
+    };
+    sonarr = {
+      enable = true;
+      group = "media";
+    };
+    readarr = {
+      enable = true;
+      group = "media";
+    };
+    nzbget = {
+      enable = true;
+      group = "media";
+      settings = {
+        ControlPassword = "";
+      };
+    };
+    audiobookshelf = {
+      enable = true;
+      group = "media";
+    };
   };
 
   services.nginx.virtualHosts."jellyfin.${config.homelab.domain}" = {
@@ -28,5 +57,72 @@
     };
   };
 
-  homelab.dashboard.Services.Jellyfin.href = "https://jellyfin.${config.homelab.domain}";
+  services.nginx.virtualHosts."radarr.${config.homelab.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.homelab.domain;
+    enableAuthelia = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:7878";
+      enableAuthelia = true;
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
+  };
+
+  services.nginx.virtualHosts."sonarr.${config.homelab.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.homelab.domain;
+    enableAuthelia = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8989";
+      enableAuthelia = true;
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
+  };
+
+  services.nginx.virtualHosts."readarr.${config.homelab.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.homelab.domain;
+    enableAuthelia = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8787";
+      enableAuthelia = true;
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
+  };
+
+  services.nginx.virtualHosts."nzbget.${config.homelab.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.homelab.domain;
+    enableAuthelia = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:6789";
+      enableAuthelia = true;
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
+  };
+
+  services.nginx.virtualHosts."audiobookshelf.${config.homelab.domain}" = {
+    forceSSL = true;
+    useACMEHost = config.homelab.domain;
+    enableAuthelia = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.audiobookshelf.port}";
+      enableAuthelia = true;
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+    };
+  };
+
+  homelab.dashboard.Media = {
+    Jellyfin.href = "https://jellyfin.${config.homelab.domain}";
+    Radarr.href = "https://radarr.${config.homelab.domain}";
+    Sonarr.href = "https://sonarr.${config.homelab.domain}";
+    Readarr.href = "https://readarr.${config.homelab.domain}";
+    NZBGet.href = "https://nzbget.${config.homelab.domain}";
+    Audiobookshelf.href = "https://audiobookshelf.${config.homelab.domain}";
+  };
 }

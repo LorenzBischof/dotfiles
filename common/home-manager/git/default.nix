d2f1175412a286f48cbacfa4c8814beaf378d512
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2024-12-15 19:53:59 +0100

    git: add jujutsu

--- common/home-manager/git/default.nix
+++ common/home-manager/git/default.nix
@@ -13,6 +13,38 @@
       executable = true;
     };
   };
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      signing = {
+        sign-all = true;
+        backend = "ssh";
+        key = "~/.ssh/id_ed25519_github.com_lorenzbischof_signing.pub";
+      };
+      user = {
+        name = "LorenzBischof";
+        email = "1837725+LorenzBischof@users.noreply.github.com";
+      };
+      colors = {
+        commit_id = "bright black";
+        author = "bright black";
+        timestamp = "bright black";
+        "working_copy commit_id" = "bright black";
+        "working_copy author" = "bright black";
+        "working_copy timestamp" = "bright black";
+      };
+      ui = {
+        diff-editor = ":builtin";
+        default-command = "log";
+      };
+      template-aliases = {
+        "format_short_change_id(id)" = "id.shortest()";
+        "format_short_commit_id(id)" = "id.short(7)";
+        "format_short_signature(signature)" = "signature.name()";
+        "format_timestamp(timestamp)" = "timestamp.ago()";
+      };
+    };
+  };
 
   programs.git = {
     enable = true;
